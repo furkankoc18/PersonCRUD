8c7dd28c8cf8d9aa8529ffe55e4bb7eb1572ec22
Furkan KOC <furkankoc18@gmail.com>
2019-02-05 23:41:52 +0300

    second commit

--- js/app.js
+++ js/app.js
@@ -1,74 +1,109 @@
-let app = angular.module('App', ['ngRoute']);
+let app = angular.module('App', ['ngRoute', 'ngStorage']);
 
 function Person(name, surname, birthYear, country, city, password, isPasive) {
-  this.name = name;
-  this.surname = surname;
-  this.birthYear = birthYear;
-  this.country = country;
-  this.city = city;
-  this.password = password;
-  this.isPasive = isPasive;
+    this.name = name;
+    this.surname = surname;
+    this.birthYear = birthYear;
+    this.country = country;
+    this.city = city;
+    this.password = password;
+    this.isPasive = isPasive;
 }
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/home', {
-      templateUrl: 'views/homepage.html',
-      controller: 'AppController'
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/homepage.html',
+            controller: 'AppController'
+        }).when('/user', {
+        templateUrl: 'views/userpage.html',
+        controller: 'AppController'
     })
-    .otherwise({
-      redirectTo: '/'
-    })
-}])
+        .otherwise({
+            redirectTo: '/'
+        })
+}]);
 
-app.controller('AppController', ['$scope', function($scope) {
+app.controller('AppController', ['$scope', '$localStorage', '$sessionStorage', function ($scope, $localStorage, $sessionStorage) {
 
-  $scope.countryList = [{
-      'countryID': 1,
-      'name': 'Türkiye'
-    },
-    {
-      'countryID': 2,
-      'name': 'Amerika'
+    let cityList = $scope.cityList;
+    $scope.personList = [];
+    $scope.personList = $localStorage.personList;
+    $scope.countryList = [
+        {
+            'countryID': 1,
+            'name': 'Türkiye'
+        },
+        {
+            'countryID': 2,
+            'name': 'Amerika'
+        }
+    ];
+
+    $scope.cityList = [
+        {
+            'cityID': 34,
+            'countryID': 1,
+            'name': 'İstanbul'
+        },
+        {
+            'cityID': 18,
+            'countryID': 1,
+            'name': 'Çankırı'
+        },
+        {
+            'cityID': 10,
+            'countryID': 2,
+            'name': 'New York'
+        },
+        {
+            'cityID': 34,
+            'countryID': 2,
+            'name': 'Chicago'
+        }
+    ];
+
+    $scope.birthYearList = [
+        {
+            'birthYearID': 1,
+            'birthYear': 1996
+        },
+        {
+            'birthYearID': 2,
+            'birthYear': 1997
+        },
+        {
+            'birthYearID': 3,
+            'birthYear': 1998
+        },
+        {
+            'birthYearID': 4,
+            'birthYear': 1999
+        }
+    ];
+
+    $scope.init = function () {
+        //init metod
+    };
+
+    $scope.addPerson = function (person) {
+        if (person.isPasive == null)
+            person.isPasive = false;
+        let personObj = new Person(person.name, person.surname, person.birthYear, person.country, person.city, person.password, person.isPasive);
+        $scope.personList.push(personObj);
+        $localStorage.personList = $scope.personList;
+    };
+
+    $scope.removePerson = function (index) {
+        $scope.personList.splice(index, 1);
+    };
+
+    $scope.changeCountry = function (countryID) {
+        $scope.cityList = [];
+        for (i = 0; i < cityList.length; i++) {
+            if (cityList[i].countryID == countryID) {
+                $scope.cityList.push(cityList[i]);
+            }
+        }
     }
-  ];
-
-  $scope.cityList = [{
-      'cityID': 34,
-      'countryID': 1,
-      'name': 'İstanbul'
-    }, {
-      'cityID': 18,
-      'countryID': 1,
-      'name': 'Çankırı'
-    },
-    {
-      'cityID': 10,
-      'countryID': 2,
-      'name': 'New York'
-    },
-    {
-      'cityID': 34,
-      'countryID': 2,
-      'name': 'Chicago'
-    }
-  ];
-  let personList = [];
-  let cityList = $scope.cityList;
-  $scope.addPerson = function(person) {
-    let personObj = new Person(person.name, person.surname, person.birthYear, person.country, person.city, person.password, person.isPasive);
-    personList.push(personObj);
-  };
-  $scope.personList = personList;
-
-  $scope.changeCountry = function(countryID) {
-    $scope.cityList = [];
-    for (i = 0; i < cityList.length; i++) {
-      if (cityList[i].countryID == countryID) {
-        $scope.cityList.push(cityList[i]);
-      }
-    }
-  }
-
-
-}])
+}]);
